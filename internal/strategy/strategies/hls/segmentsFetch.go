3c6219d6701f9eb9d8b4dd52662bd698f8274f19
Antoni Jaros <antonijaros04@gmail.com>
2026-07-27 20:45:25 +0200

    Refactor: improve segment downloading and error handling in getAllSegments function

--- internal/strategy/strategies/hls/segmentsFetch.go
+++ internal/strategy/strategies/hls/segmentsFetch.go
@@ -8,19 +8,40 @@ import (
 	"github.com/Eyevinn/hls-m3u8/m3u8"
 )
 
-const errorChanSize = 1
-
 const filePattern = "*.part"
 
 func getAllSegments(playlist *m3u8.MediaPlaylist, playlistURL string) ([]string, error) {
+	var wg sync.WaitGroup
+	hasMap := playlist.Map != nil
+	count := playlist.Count()
+	if hasMap {
+		count += 1
+	}
+	paths := make([]string, count)
+	segmentPaths := paths
+	errorsCh := make(chan error, 1)
+
+	if hasMap {
+		segmentPaths = paths[1:]
+		wg.Go(func() {
+			mapPath, err := downloadMap(playlist.Map.URI, playlistURL)
+			if err != nil {
+				select {
+				case errorsCh <- err:
+				default:
+				}
+
+				return
+			}
+
+			paths[0] = mapPath
+		})
+	}
+
 	keys, err := getAllKeys(playlist, playlistURL)
 	if err != nil {
 		return nil, err
 	}
-
-	var wg sync.WaitGroup
-	paths := make([]string, playlist.Count())
-	errorsCh := make(chan error, errorChanSize)
 
 	for i, segment := range playlist.GetAllSegments() {
 		wg.Go(func() {
@@ -34,7 +55,7 @@ func getAllSegments(playlist *m3u8.MediaPlaylist, playlistURL string) ([]string,
 				return
 			}
 
-			paths[i] = segmentPath
+			segmentPaths[i] = segmentPath
 		})
 	}
 
@@ -43,7 +64,7 @@ func getAllSegments(playlist *m3u8.MediaPlaylist, playlistURL string) ([]string,
 		close(errorsCh)
 	}()
 
-	for err := range errorsCh {
+	if err, ok := <-errorsCh; ok {
 		return nil, err
 	}
 
@@ -53,13 +74,8 @@ func getAllSegments(playlist *m3u8.MediaPlaylist, playlistURL string) ([]string,
 func downloadSegment(segment *m3u8.MediaSegment, playlistURL string, decryption decrpytionInfo) (string, error) {
 	var path string
 
-	segmentBuf := bytes.Buffer{}
-	segmentURL, err := resolveAbsoluteURL(playlistURL, segment.URI)
+	segmentBuf, err := resolveURLAndDownload(playlistURL, segment.URI)
 	if err != nil {
-		return path, err
-	}
-
-	if err := getResponseBody(segmentURL, &segmentBuf); err != nil {
 		return path, err
 	}
 
@@ -67,13 +83,59 @@ func downloadSegment(segment *m3u8.MediaSegment, playlistURL string, decryption 
 	if err != nil {
 		return path, err
 	}
+
+	path, err = createTempFile(decryptedSegment)
+	if err != nil {
+		return path, err
+	}
+
+	return path, nil
+}
+
+func downloadMap(mapURL, playlistURL string) (string, error) {
+	var path string
+
+	mapData, err := resolveURLAndDownload(playlistURL, mapURL)
+	if err != nil {
+		return path, err
+	}
+
+	path, err = createTempFile(mapData.Bytes())
+	if err != nil {
+		return path, err
+	}
+
+	return path, nil
+}
+
+func resolveURLAndDownload(baseURL, relativeURL string) (*bytes.Buffer, error) {
+	dataBuf := &bytes.Buffer{}
+
+	dataURL, err := resolveAbsoluteURL(baseURL, relativeURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := getResponseBody(dataURL, dataBuf); err != nil {
+		return nil, err
+	}
+
+	return dataBuf, nil
+}
+
+func createTempFile(data []byte) (string, error) {
+	var path string
+
 	f, err := os.CreateTemp("", filePattern)
 	if err != nil {
 		return path, err
 	}
+
 	defer f.Close()
+
 	path = f.Name()
-	if _, err := f.Write(decryptedSegment); err != nil {
+
+	if _, err := f.Write(data); err != nil {
 		return path, err
 	}
 
